ae01c3aa779d24766e3d9286f1f95b66d9754290
Preet1406 <14preet2008@gmail.com>
2022-11-10 13:36:46 +0530

    linking home

--- src/authsysproject/users/urls.py
+++ src/authsysproject/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.showImage, name='home'),
     path('register/', views.register, name='register'),
     path('upload/', views.upload, name='upload'),
+    path('home/', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
