2741044cccc00d2a1b378d519cd30013f392e4ea
pranaykumar247 <pranaykumar074@gmail.com>
2022-11-11 11:24:25 +0530

    URLS update

--- src/authsysproject/users/urls.py
+++ src/authsysproject/users/urls.py
@@ -3,10 +3,10 @@ from . import views
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    path('', views.showImage, name='home'),
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('upload/', views.upload, name='upload'),
-    path('home/', views.home, name='home'),
+    # path('home/', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
