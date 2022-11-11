72f62de362e3867de141bb6fbf1377aedb99d9a7
pranaykumar247 <pranaykumar074@gmail.com>
2022-11-11 11:05:16 +0530

    Minor UI changes

--- src/authsysproject/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Image
-# Register your models here.
-
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ['id','photo','date']
-    
-
