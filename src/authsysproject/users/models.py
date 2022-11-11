df99c2c8f76d4cff20aa422f78292048d9b153e5
pranaykumar247 <pranaykumar074@gmail.com>
2022-11-11 10:23:23 +0530

    Galleria Update

--- src/authsysproject/users/models.py
+++ src/authsysproject/users/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    # title = models.CharField(default,max_length=100)
+    # title = models.CharField(max_length=100)
     photo = models.ImageField(upload_to="myimage")
     date = models.DateTimeField(auto_now_add=True)
+    
+    # def __str__(self):
+    #         return self.title
+    
 
 
 
--- src/authsysproject/users/urls.py
+++ src/authsysproject/users/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name="login"),
-    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/home.html'), name="logout"),
 ]
